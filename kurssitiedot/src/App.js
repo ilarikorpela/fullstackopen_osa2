c4506cb6f272abc3b93582d9f6d345742e4c03c4
Ilari <ilari.tkorpela@gmail.com>
2020-09-22 18:34:39 +0300

    2.1

--- kurssitiedot/src/App.js
+++ kurssitiedot/src/App.js
@@ -1,61 +1,26 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import logo from "./logo.svg";
+import "./App.css";
 
-const Header = (props) => {
-  return <h1>{props.course}</h1>;
-};
-
-const Part = (props) => {
+function App() {
   return (
-    <p>
-      {props.part.name} {props.part.exercises}
-    </p>
-  );
-};
-
-const Content = (props) => {
-  const parts = props.parts;
-  const components_part = parts.map((part) => (
-    <Part key={part.id} part={part} />
-  ));
-  return <>{components_part}</>;
-};
-const Course = (props) => {
-  return (
-    <div>
-      <Header course={props.course_json.name} />
-      <Content parts={props.course_json.parts} />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-};
+}
 
-const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-        id: 3,
-      },
-    ],
-  };
-
-  return (
-    <div>
-      <Course course_json={course} />
-    </div>
-  );
-};
-
-ReactDOM.render(<App />, document.getElementById("root"));
+export default App;
